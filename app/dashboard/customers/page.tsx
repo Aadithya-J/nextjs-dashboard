40a48035aba673fba3673776e6b9785c505bc29a
Aadithya-J <jlaadithya@gmail.com>
2024-03-17 12:01:44 +0530

    added sidebar and dashboard,invoice and customers page

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function customerPage(){
+    return(
+        <p>Customer Page</p>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function invoicePage(){
+    return(
+        <p>Invoice Page</p>
+    )
+}
